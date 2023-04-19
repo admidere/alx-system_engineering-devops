b899f3821a864edf9409c8abf2eb4bddfb220db1
admidere <111120975+admidere@users.noreply.github.com>
2023-04-19 07:19:50 -0700

    delete

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# set up your client SSH configuration file
-
-exec { 'ssh_config':
-  path    => '/bin/',
-  command => 'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config'
-}
