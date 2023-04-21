8aef85a989d1cb23802ba6d5866aec65e558afea
admidere <admasud89@gmail.com>
2023-04-21 00:08:53 -0700

    configuration managment

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# Creates a file in /tmp named "school" with the specified permissions, owner, group, and content
+file { '/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,7 @@
+# Kills a process named "killmenow"
+exec { 'killmenow':
+  command     => 'pkill killmenow',
+  path        => '/usr/bin/pkill -f killmenow',
+  onlyif      => 'pgrep killmenow',
+  refreshonly => true,
+}
