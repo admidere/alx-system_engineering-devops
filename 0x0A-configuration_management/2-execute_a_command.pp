9afd35d503a00b41f1d3966ea5150d72dc3951f0
admidere <admasud89@gmail.com>
2023-04-21 11:30:16 -0700

    execute a command

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # Kills a process named "killmenow"
 exec { 'killmenow':
   command     => 'pkill killmenow',
-  path        => '/usr/bin/pkill -f killmenow',
+  path        => ['/usr/bin', '/bin', '/usr/local/bin'],
   onlyif      => 'pgrep killmenow',
 }
