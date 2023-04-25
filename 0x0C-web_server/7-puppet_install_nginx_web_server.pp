4bcf3ec0d3c8db8d674bae41ed5210dd03519b99
admidere <111120975+admidere@users.noreply.github.com>
2023-04-25 10:56:40 -0700

    puppet_install_nginx_web_server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,34 +1,32 @@
 # Install and configure Nginx web server using puppet
 
-class nginx_server {
-  package { 'nginx':
-    ensure => 'installed',
-  }
+package { 'nginx':
+  ensure => installed,
+}
 
-  service { 'nginx':
-    ensure => 'running',
-    enable => true,
-    require => Package['nginx'],
-  }
+service { 'nginx':
+  ensure => running,
+  enable => true,
+  require => Package['nginx'],
+}
 
-  file { '/var/www/html/index.html':
-    content => 'Hello World!',
-    require => Package['nginx'],
-  }
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+  require => Package['nginx'],
+}
 
-  file { '/etc/nginx/sites-available/default':
-    content => "server {
-      listen 80 default_server;
-      listen [::]:80 default_server;
-      server_name _;
-      return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4$request_uri;
-    }",
-    require => Package['nginx'],
-  }
+file { '/etc/nginx/sites-available/default':
+  content => "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    server_name _;
+    return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4$request_uri;
+  }",
+  require => Package['nginx'],
+}
 
-  file { '/etc/nginx/sites-enabled/redirect':
-    ensure => 'link',
-    target => '/etc/nginx/sites-available/default',
-    require => File['/etc/nginx/sites-available/default'],
-  }
+file { '/etc/nginx/sites-enabled/redirect':
+  ensure => 'link',
+  target => '/etc/nginx/sites-available/default',
+  require => File['/etc/nginx/sites-available/default'],
 }
