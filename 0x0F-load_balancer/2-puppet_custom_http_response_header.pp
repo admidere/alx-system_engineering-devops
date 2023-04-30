97f33568d50d7d47fb700283a3074962318615d1
admidere <111120975+admidere@users.noreply.github.com>
2023-04-30 03:04:54 -0700

    puppet_custom_http_response_header

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,39 +1,8 @@
 # Ensure the 'apt' module is installed
-exec { 'install_apt_module':
-  command => 'puppet module install puppetlabs-apt',
-  path    => '/usr/bin:/usr/sbin:/bin:/sbin',
-  unless  => 'puppet module list | grep -q puppetlabs-apt',
-}
-
-# Update package lists
-class { 'apt':
-  update => {
-    frequency => 'daily',
-  },
-  require => Exec['install_apt_module'],
-}
-
-# Install Nginx
-class { 'nginx':
-  manage_repo    => true,
-  package_source => 'nginx-mainline',
-  require        => Class['apt::update'],
-}
-
-# Configure Nginx with custom header
-nginx::resource::server { 'default':
-  listen_options => 'default_server',
-  www_root       => '/var/www/html',
-  index_files    => ['index.html'],
-  use_default_location => false,
-  locations      => {
-    '/' => {
-      location => '/',
-      index_files => ['index.html'],
-      custom_cfg => {
-        'add_header' => 'X-Served-By $hostname',
-      },
-    },
-  },
-  require        => Class['nginx'],
+exec {'Custom HTTP header':
+  command  => 'sudo apt-get update;
+    sudo apt-get -y install nginx;
+    sudo sed -i "/Default server configuration/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+    sudo service nginx restart',
+  provider => shell,
 }
