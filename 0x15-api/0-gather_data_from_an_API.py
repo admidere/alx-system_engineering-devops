3782b392e5b54b7230da2774a016b2556c3556d7
admidere <admasud89@gmail.com>
2023-05-22 09:24:45 -0700

    gather data from api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,38 +1,49 @@
-#!/usr/bin/python3
-
+#!/usr/bin/env python3
 """
-This script talks to jsonplaceholder api to fetch
-data about a user that includes the name of the user from
-/users api endpoint and todo lists from todos?userId={} api
-endpoint
+This script fetches an employee's TODO list progress from a REST API and displays it in the specified format.
 """
 
-
-import requests
 import sys
+import requests
 
 
-if __name__ == '__main__':
-    url = "https://jsonplaceholder.typicode.com/"
-    user_id = sys.argv[1]
+def get_employee_todo_progress(employee_id):
+    """
+    Fetches and displays an employee's TODO list progress from a REST API.
 
-    user_response = requests.get(url + "users/{}".format(user_id))
-    if user_response.status_code != 200:
-        print("Error: User not found")
+    Args:
+        employee_id (int): The employee ID.
+    """
+    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+
+    user_response = requests.get(user_url)
+    todos_response = requests.get(todos_url)
+
+    if user_response.status_code != 200 or todos_response.status_code != 200:
+        print("Error: Unable to fetch data from the API.")
+        return
+
+    user_data = user_response.json()
+    todos_data = todos_response.json()
+
+    employee_name = user_data.get("name")
+    done_tasks = [task for task in todos_data if task.get("completed")]
+    total_tasks = len(todos_data)
+
+    print(f"Employee {employee_name} is done with tasks({len(done_tasks)}/{total_tasks}):")
+    for task in done_tasks:
+        print(f"\t {task.get('title')}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
         sys.exit(1)
 
-    user = user_response.json()
-    todos_response = requests.get(url + "todos?userId={}".format(user_id))
-    if todos_response.status_code != 200:
-        print("Error: Failed to fetch todos")
+    try:
+        employee_id = int(sys.argv[1])
+    except ValueError:
+        print("Error: Employee ID must be an integer.")
         sys.exit(1)
 
-    todos = todos_response.json()
-
-    completed = [t.get("title") for t in todos if t.get("completed")]
-    completed_tasks = len(completed)
-    total_tasks = len(todos)
-    print("Employee Name: {}".format(user.get("name")))
-    print("({}/{}):".format(completed_tasks, total_tasks))
-    for task in completed:
-        print("\t{}".format(task))
+    get_employee_todo_progress(employee_id)
