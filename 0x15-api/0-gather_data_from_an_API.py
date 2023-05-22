b77df6f0645059e96b0f03d87fdd8f6192d3b575
admidere <admasud89@gmail.com>
2023-05-22 03:21:30 -0700

    gather data from api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Python script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress."""
+
+import sys
+import requests
+
+
+def get_employee_todo_progress(employee_id):
+    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+
+    user_response = requests.get(user_url)
+    todos_response = requests.get(todos_url)
+
+    if user_response.status_code != 200 or todos_response.status_code != 200:
+        print("Error: Unable to fetch data from the API.")
+        return
+
+    user_data = user_response.json()
+    todos_data = todos_response.json()
+
+    employee_name = user_data["name"]
+    done_tasks = [task for task in todos_data if task["completed"]]
+    total_tasks = len(todos_data)
+
+    print(f"Employee {employee_name} is done with tasks({len(done_tasks)}/{total_tasks}):")
+    for task in done_tasks:
+        print(f"\t {task['title']}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+    get_employee_todo_progress(employee_id)
