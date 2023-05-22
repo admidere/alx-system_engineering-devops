65dbe60276c1c68be35aa901770807cb683f36f4
admidere <admasud89@gmail.com>
2023-05-22 09:02:06 -0700

    gather data from api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python3
+
 """
-Python script that, using this REST API, for a given employee ID
-returns information about his/her TODO list progress.
+This script talks to jsonplaceholder api to fetch
+data about a user that includes the name of the user from
+/users api endpoint and todo lists from todos?userId={} api
+endpoint
 """
+
+
+import requests
+import sys
 
 
 if __name__ == '__main__':
-    import requests
-    from sys import argv
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos?userId={}".format(sys.argv[1])).json()
 
-    user_address = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                                .format(argv[1]))
-    name = user_address.json().get('name')
-    tod = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
-                       .format(argv[1]))
-    request_data = tod.json()
-    done = total = 0
-    for task in request_data:
-        total += 1
-        if task.get('completed'):
-            done += 1
-
-    print('Employee {} is done with tasks({}/{}):'.format(name, done, total))
-    for task in request_data:
-        if task.get('completed'):
-            print('\t {}'.format(task.get('title')))
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
